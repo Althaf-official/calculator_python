6cb6b85f6e84fb76889bf799e3917bb720d4ba18
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-17 17:51:18 +0400

    sample calculator completed

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
-from tkinter import *
-# if you put "*" you can use all attributes from tkinter
+import tkinter as tk
 
+def calculate():
+    try:
+        result = eval(entry.get())
+        output.config(text="Result: " + str(result))
+    except Exception as e:
+        output.config(text="Error: " + str(e))
+
+# Create the main window
+window = tk.Tk()
+window.title("Calculator")
+
+# Create the entry field
+entry = tk.Entry(window)
+entry.pack()
+
+# Create the calculate button
+button = tk.Button(window, text="Calculate", command=calculate)
+button.pack()
+
+# Create the output label
+output = tk.Label(window)
+output.pack()
+
+# Start the main loop
+window.mainloop()
